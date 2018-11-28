b7fb8b0b3265e411a5c4a8f24288c10175c466ad
OmarCharlesGedeon <charles@effortlesshost.com>
2018-11-28 09:55:02 -0800

    Html/bootstrap setup. Still having issues with getting the image in the proper placement. Created Pseudocode for my Js logic.

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,74 @@
+  // Initialize Firebase
+
+  // Create a variable to reference the database
+
+  //Run Time  
+
+  // Capture Button Click
+
+  // Don't refresh the page!
+
+  // Code in the logic for storing and retrieving the most recent train information
+
+  // Don't forget to provide initial data to your Firebase database. Set replaces old data
+
+  // Alert
+  
+  // alert("Train was successfully added");
+
+  // Clears all of the text-boxes
+
+  // Firebase watcher + initial loader 
+
+  // determine current time
+
+  // get timer function
+
+  // Difference between times
+
+  // Time apart (remainder)
+
+  //determine Minutes Away
+
+  //determine Next Train Arrival
+
+  //want to push to table to add new train 
+  
+  //add new table row
+  
+  //add new train information into row
+  
+  //add each train's data into the table row
+
+  //add back updated information
+
+  // var t = setTimeout(startTime, 500);
+
+  // If any errors are experienced, log them to console.
+
+  // on click for deleting row if trash is clicked
+  
+  //Update time of minutesAway and nextArrival 
+
+  // Update minutes away by triggering change in firebase
+
+  //empty tbody before appending new information
+
+  // Current Time
+
+  // Difference between the times
+
+  // Time apart (remainder)
+
+  // determine minutes away
+
+  // determine next train arrival
+
+  // want to push to table to add new train 
+  
+  // add new table row
+  
+  // add new train information into row
+  
+  // Add each train's data into the table row
+
